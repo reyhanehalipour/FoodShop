fa0c00dfbf9f4daa97198d64348e92519d71fa91
Reyhane <reyhanehalipour78@gmail.com>
2022-06-17 02:25:10 -0700

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import'./assets/css/bootstrap-rtl.min.css';
+import './assets/css/bootstrap.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
